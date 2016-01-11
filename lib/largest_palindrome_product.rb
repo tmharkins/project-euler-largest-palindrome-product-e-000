87b59ba3de0e57720b4279a1eb8f40226c235fc6
tmharkins <tmharkins@gmail.com>
2016-01-11 04:46:25 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,22 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  a = 999
+	x = 1 #outer loop counter
+	largest = 0
+  product = 0
+
+	while x <= a
+		y = 1 #reset inner loop
+		while y <= a
+			product = x * y
+			prod = product.to_s
+			if prod == prod.reverse
+				if prod.to_i > largest
+					largest = product
+				end
+			end
+			y += 1 #increment inne rloop
+		end
+		x += 1 #increment outer loop
+	end
+  return largest
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,54 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def initialize
+    @a = 999        #highest number to be multiplied
+    @x = 1        #outer loop counter
+    @y = 0        #inner loop counter
+    @largest = 0  #largest product that is a palindrome
+  end
+
+  def answer
+    while @x < @a || @y < @a
+      vars_x_and_y              #determine x and y
+      product                   #multiply x and y
+      is_largest_palindrome?    #determine new/largest palindrome
+    end
+    return @largest
+  end
+
+  def vars_x_and_y
+    if @x > @a
+      @x, @y = 0
+    elsif @y >= @a
+      @y = 0
+      @x += 1
+    end
+    @y += 1
+  end
+
+  def product
+    @x * @y
+  end
+
+  def is_a_palindrome?
+    prod = product.to_s
+    prod == prod.reverse
+  end
+
+  def is_new_largest_palindrome?
+    if is_a_palindrome?
+      product > @largest ? @largest = product : nil
+      return true
+    end
+  end
+
+  def is_largest_palindrome?
+    if is_new_largest_palindrome? && loop_complete?
+      @largest_palindrome_found = true
+    end
+  end
+
+  def loop_complete?
+    @x == @a && @y == @a
+  end
+end
